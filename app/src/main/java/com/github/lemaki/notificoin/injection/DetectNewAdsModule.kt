11d7f941f2ba246c5769c7969b90195138fec37e
Corentin Chatellier <cchatellier.prestataire@oui.sncf>
2020-02-20 09:45:37 +0100

    [POC] inject detectNewAdsInteractor

---
+++ app/src/main/java/com/github/lemaki/notificoin/injection/DetectNewAdsModule.kt
@@ -0,0 +1,9 @@
+package com.github.lemaki.notificoin.injection
+
+import com.github.lemaki.notificoin.domain.DetectNewAdsInteractor
+import com.github.lemaki.notificoin.ui.detectNewAds.DetectNewAdsPresenter
+import org.koin.dsl.module
+
+val detectNewAdsModule = module {
+    single { (detectNewAdsPresenter: DetectNewAdsPresenter) -> DetectNewAdsInteractor(get(), detectNewAdsPresenter) }
+}
