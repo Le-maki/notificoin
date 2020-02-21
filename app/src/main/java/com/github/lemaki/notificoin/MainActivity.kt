9c429fbf76f5cced219d9e0f31a0067220224111
Corentin Chatellier <cchatellier.prestataire@oui.sncf>
2020-02-21 17:34:38 +0100

    [POC] ask user to whitelist the app to avoid sockettimeout, update some dependencies

--- app/src/main/java/com/github/lemaki/notificoin/MainActivity.kt
+++ app/src/main/java/com/github/lemaki/notificoin/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.github.lemaki.notificoin
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
@@ -32,15 +41,15 @@ import org.koin.core.error.KoinAppAlreadyStartedException
 
 class MainActivity: AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		try {
-			startKoin {
-				androidLogger()
-				fragmentFactory()
-				androidContext(this@MainActivity)
-				modules(
-					listOf(
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        try {
+            startKoin {
+                androidLogger()
+                fragmentFactory()
+                androidContext(this@MainActivity)
+                modules(
+                    listOf(
                         homeModule,
                         databaseModule,
                         adModule,
@@ -50,31 +59,70 @@ class MainActivity: AppCompatActivity() {
                         alarmManagerModule,
                         searchWithAdsModule,
                         detectNewAdsModule
-					)
-				)
-			}
-		} catch (exception: KoinAppAlreadyStartedException) {
-			NotifiCoinLogger.i(this.applicationContext.resources.getString(R.string.koin_already_started))
-		}
+                    )
+                )
+            }
+        } catch (exception: KoinAppAlreadyStartedException) {
+            NotifiCoinLogger.i(this.applicationContext.resources.getString(R.string.koin_already_started))
+        }
 
-		setContentView(R.layout.activity_main)
-		val navController = findNavController(R.id.navHostFragment)
-		val appBarConfiguration = AppBarConfiguration(
-			setOf(
-				R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-			)
-		)
-		setupActionBarWithNavController(navController, appBarConfiguration)
-		navView.setupWithNavController(navController)
-		SoLoader.init(this, false)
+        setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        SoLoader.init(this, false)
 
-		if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-			val client = AndroidFlipperClient.getInstance(this)
-			client.addPlugin(InspectorFlipperPlugin(this.applicationContext, DescriptorMapping.withDefaults()))
-			val networkFlipperPlugin = NetworkFlipperPlugin()
-			client.addPlugin(DatabasesFlipperPlugin(this))
-			client.addPlugin(networkFlipperPlugin)
-			client.start()
-		}
-	}
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this.applicationContext, DescriptorMapping.withDefaults()))
+            val networkFlipperPlugin = NetworkFlipperPlugin()
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.addPlugin(networkFlipperPlugin)
+            client.start()
+        }
+        requestBatteryWhiteListPermission(this@MainActivity)
+    }
+
+    private fun requestBatteryWhiteListPermission(context: Context) {
+        val powerManager = context.getSystemService(Context.POWER_SERVICE) as? PowerManager
+        if (powerManager != null && !powerManager.isIgnoringBatteryOptimizations(packageName)) {
+            val alertMessage = getString(R.string.battery_white_list_explanation)
+            val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+            val view: View = View.inflate(context, R.layout.battery_whitelist_alertdialog, null)
+            Glide.with(context)
+                    .load(R.raw.battery_whitelist)
+                    .into(view.findViewById(R.id.batteryWhiteListGif))
+            builder.setView(view)
+            builder.setMessage(alertMessage)
+            builder.setPositiveButton("OK") { _, _ ->
+                try {
+                    goToBatteryWhiteListOfTheApp(context)
+                } catch (exception: ActivityNotFoundException) {
+                    goToBatteryWhiteList()
+                }
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+
+    }
+
+    private fun goToBatteryWhiteListOfTheApp(context: Context) {
+        val intent = Intent()
+        intent.action = Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+        val packageName: String = context.packageName
+        intent.data = Uri.parse("package:$packageName")
+        startActivity(intent)
+    }
+
+    private fun goToBatteryWhiteList() {
+        val intent = Intent()
+        intent.action = Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+        startActivity(intent)
+    }
 }
