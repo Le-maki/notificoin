31d85ea37d83e29a86d81f082dbf5b7411f535bc
Corentin Chatellier <cchatellier.prestataire@oui.sncf>
2020-05-06 17:21:52 +0200

    [develop] use JetPack Preference in SettingsFragment

--- app/src/main/java/com/github/corentinc/notificoin/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/github/corentinc/notificoin/ui/settings/SettingsFragment.kt
@@ -1,38 +1,27 @@
 package com.github.corentinc.notificoin.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceFragmentCompat
 import com.github.corentinc.core.SettingsInteractor
 import com.github.corentinc.notificoin.R
-import com.github.corentinc.notificoin.ui.ChildFragment
-import kotlinx.android.synthetic.main.fragment_settings.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SettingsFragment(
     private val settingsInteractor: SettingsInteractor
-): ChildFragment(), SettingsDisplay {
+): PreferenceFragmentCompat(), SettingsDisplay {
     private val settingsViewModel: SettingsViewModel by viewModel()
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        bindViewModel()
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
 
     init {
         (settingsInteractor.settingsPresenter as SettingsPresenterImpl).settingsDisplay = this
     }
 
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_screen, rootKey)
+    }
+
     override fun onStart() {
-        settingsFragmentAboutText.setOnClickListener {
-            findNavController().navigate(R.id.action_navigation_settings_to_navigation_about)
-        }
+        bindViewModel()
         settingsInteractor.onStart()
         super.onStart()
     }
@@ -41,7 +30,7 @@ class SettingsFragment(
         settingsViewModel.text.observe(
             viewLifecycleOwner,
             Observer {
-                settingsFragmentAboutText.text = it
+                // settingsFragmentAboutText.text = it
             })
     }
 }
