06afe2d1b76a3a31d528756abb8f42ed1d42d92f
Corentin Chatellier <cchatellier.prestataire@oui.sncf>
2020-05-17 20:59:24 +0200

    [develop] remove default url

--- core/src/main/java/com/github/corentinc/core/home/HomeInteractor.kt
+++ core/src/main/java/com/github/corentinc/core/home/HomeInteractor.kt
@@ -23,11 +23,7 @@ class HomeInteractor(
     private val searchAdsPositionRepository: SearchAdsPositionRepository,
     private val sharedPreferencesRepository: SharedPreferencesRepository,
     private val globalSharedPreferencesRepository: GlobalSharedPreferencesRepository,
-    private val searchAdsPostionDefaultSorter: SearchAdsPostionDefaultSorter,
-    private val searches: Map<String, String> = mapOf(
-        "https://www.leboncoin.fr/recherche/?category=2&locations=Nantes&regdate=2010-max" to "Voiture",
-        "https://www.leboncoin.fr/recherche/?text=jeu%20switch&locations=Nantes__47.23898554566441_-1.5262136157260586_10000" to "Jeu Switch"
-    )
+    private val searchAdsPostionDefaultSorter: SearchAdsPostionDefaultSorter
 ) {
 
     fun onStart(
@@ -43,12 +39,6 @@ class HomeInteractor(
         }
         CoroutineScope(Dispatchers.IO).launch {
             var searchAdsPosition = searchAdsPositionRepository.getAllSortedSearchAdsPosition()
-            if (searchAdsPosition.isEmpty()) {
-                searches.forEach {
-                    searchRepository.addSearch(Search(title = it.value, url = it.key))
-                }
-                searchAdsPosition = searchAdsPositionRepository.getAllSortedSearchAdsPosition()
-            }
             withContext(Dispatchers.Main) {
                 searchAdsPosition = searchAdsPosition.sortedWith(searchAdsPostionDefaultSorter)
                 homePresenter.presentSearches(searchAdsPosition.map { it.search }.toMutableList())
