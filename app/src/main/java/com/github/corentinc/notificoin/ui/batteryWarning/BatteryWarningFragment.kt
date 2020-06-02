43e64e57b962d35a5832d30e3754513636be8097
Corentin Chatellier <cchatellier.prestataire@oui.sncf>
2020-06-02 19:20:43 +0200

    [develop] hide gif in BatteryWarning alertDialog when device is in landscape

--- app/src/main/java/com/github/corentinc/notificoin/ui/batteryWarning/BatteryWarningFragment.kt
+++ app/src/main/java/com/github/corentinc/notificoin/ui/batteryWarning/BatteryWarningFragment.kt
@@ -1,19 +1,24 @@
 package com.github.corentinc.notificoin.ui.batteryWarning
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.provider.Settings
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.github.corentinc.core.BatteryWarningInteractor
 import com.github.corentinc.notificoin.R
 import com.github.corentinc.notificoin.ui.ChildFragment
+import kotlinx.android.synthetic.main.battery_whitelist_alertdialog.*
 
 
 class BatteryWarningFragment(
@@ -50,6 +55,8 @@ class BatteryWarningFragment(
         view.findViewById<Button>(R.id.batteryWhiteListMaybeButton).setOnClickListener {
             requireActivity().onBackPressed()
         }
+        view.findViewById<ImageView>(R.id.batteryWhiteListGif).isVisible =
+            activity?.resources?.configuration?.orientation != Configuration.ORIENTATION_LANDSCAPE
     }
 
     override fun onPause() {
