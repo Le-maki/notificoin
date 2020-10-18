80131c84996d63a0f421ccb134d19d6a4322b0e8
c.chatellier <c.chatellier@groupeonepoint.com>
2020-10-18 21:49:30 +0200

    [develop] fix ad repository because of "ads_shippable"

--- repository/src/main/java/com/github/corentinc/repository/webpage/DocumentToAdJsonArrayTransformer.kt
+++ repository/src/main/java/com/github/corentinc/repository/webpage/DocumentToAdJsonArrayTransformer.kt
@@ -10,8 +10,8 @@ import java.util.regex.Pattern
 class DocumentToAdJsonArrayTransformer {
     companion object {
         private const val DATA_REGEX = "REDIAL_PROPS__ = (\\[.*)"
-        private const val AD_LIST_REGEX = "(\\{\"list_id(.*?))((,\"ads_alu.*?\$)|\$)"
-        private const val AD_REGEX = "(\\{\"list_id(.*?))(?=,\\{\"list_id)"
+        private const val AD_REGEX =
+            "(\\{\"list_id(.*?))(?=(,\\{\"list_id)|(,\"ads_shippable)|(,\"ads_alu))"
         private const val TAG_NAME = "script"
         private const val NODE_SELECTOR = "list_id"
     }
@@ -25,20 +25,20 @@ class DocumentToAdJsonArrayTransformer {
         val dataMatcher = Pattern.compile(DATA_REGEX).matcher(dataNode.toString())
         dataMatcher.find()
         dataMatcher.group(1)?.let { data ->
-            val adListMatcher = Pattern.compile(AD_LIST_REGEX).matcher(data)
-            adListMatcher.find()
-            val adList = adListMatcher.group(1)
-            adList?.let {
-                val adMatcher = Pattern.compile(AD_REGEX).matcher(it)
+            val adMatcher = Pattern.compile(AD_REGEX).matcher(data)
                 while (adMatcher.find()) {
+                    try {
                         adJsonArray.add(
                             Gson().fromJson(
                                 adMatcher.group(1),
                                 JsonElement::class.java
                             )
                         )
+                    } catch (exception: Exception) {
+                        println()
+                    }
+
                 }
-            } ?: throw ParseException("Unable to parse the webpage", 0)
         } ?: throw ParseException("Unable to parse the webpage", 0)
         return adJsonArray
     }
