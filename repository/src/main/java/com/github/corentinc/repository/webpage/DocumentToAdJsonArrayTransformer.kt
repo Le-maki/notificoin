46e8943b2df74929ffb2a0ceb63578140b0104c7
c.chatellier <c.chatellier@groupeonepoint.com>
2020-10-18 22:36:00 +0200

    [develop] fix ad repository do not include "ads_shippable" at all

--- repository/src/main/java/com/github/corentinc/repository/webpage/DocumentToAdJsonArrayTransformer.kt
+++ repository/src/main/java/com/github/corentinc/repository/webpage/DocumentToAdJsonArrayTransformer.kt
@@ -10,6 +10,7 @@ import java.util.regex.Pattern
 class DocumentToAdJsonArrayTransformer {
     companion object {
         private const val DATA_REGEX = "REDIAL_PROPS__ = (\\[.*)"
+        private const val AD_LIST_REGEX = "(\\{\"list_id(.*?))((,\"ads_alu)|(,\"ads_shippable)|$)"
         private const val AD_REGEX =
             "(\\{\"list_id(.*?))(?=(,\\{\"list_id)|(,\"ads_shippable)|(,\"ads_alu))"
         private const val TAG_NAME = "script"
@@ -25,7 +26,11 @@ class DocumentToAdJsonArrayTransformer {
         val dataMatcher = Pattern.compile(DATA_REGEX).matcher(dataNode.toString())
         dataMatcher.find()
         dataMatcher.group(1)?.let { data ->
-            val adMatcher = Pattern.compile(AD_REGEX).matcher(data)
+            val adListMatcher = Pattern.compile(AD_LIST_REGEX).matcher(data)
+            adListMatcher.find()
+            val adList = adListMatcher.group(1)
+            adList?.let {
+                val adMatcher = Pattern.compile(AD_REGEX).matcher(it)
                 while (adMatcher.find()) {
                     try {
                         adJsonArray.add(
@@ -39,6 +44,7 @@ class DocumentToAdJsonArrayTransformer {
                     }
 
                 }
+            } ?: throw ParseException("Unable to parse the webpage", 0)
         } ?: throw ParseException("Unable to parse the webpage", 0)
         return adJsonArray
     }
